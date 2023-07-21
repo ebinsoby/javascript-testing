528568b682c0e028083c1d62aa85eed0f33dd47f
Ebin Soby <mailebinsoby@gmail.com>
2023-07-21 16:46:05 -0400

    setting up logs to identify each tests

--- testing-frameworks.js
+++ testing-frameworks.js
@@ -2,12 +2,12 @@ let result, expected;
 function sum(a,b){
     return a+b;
 }
-
-result = sum(3,5);
-expected = 10;
-
-expect(result).toBe(expected);
-
+function takingSum(){
+    result = sum(3,5);
+    expected = 8;
+    expect(result).toBe(expected);
+}
+test("checking sum", takingSum);
 
 function expect(actual){
 return {
@@ -17,4 +17,13 @@ return {
         }
     }
 }
+}
+function test(title, callback){
+    try {
+        callback();
+        console.log(`✅ ${title}`)
+    } catch (error) {
+        console.error(`❌ ${title}`)
+        console.error(error)
+    }
 }
